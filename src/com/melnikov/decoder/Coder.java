8380358b1e6b1fc7ee76160d4eb216202f65f55d
nikita <melnikov.n.89@mail.ru>
2021-08-22 09:32:01 +0300

    Initial commit

---
+++ src/com/melnikov/decoder/Coder.java
@@ -0,0 +1,93 @@
+package com.melnikov.decoder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Coder {
+    public static void codeFile(Path input, Path output, int shift) throws IOException {
+        String plaintext = Files.readString(input);
+        String ciphertext = "";
+        char alphabet;
+
+        for(int i=0; i < plaintext.length();i++)
+        {
+            // Shift one character at a time
+            alphabet = plaintext.charAt(i);
+
+            // if alphabet lies between a and z
+            if(alphabet >= 'a' && alphabet <= 'z')
+            {
+                // shift alphabet
+                alphabet = (char) (alphabet + shift);
+                // if shift alphabet greater than 'z'
+                if(alphabet > 'z') {
+                    // reshift to starting position
+                    alphabet = (char) (alphabet+'a'-'z'-1);
+                }
+                ciphertext = ciphertext + alphabet;
+            }
+
+            // if alphabet lies between 'A'and 'Z'
+            else if(alphabet >= 'A' && alphabet <= 'Z') {
+                // shift alphabet
+                alphabet = (char) (alphabet + shift);
+
+                // if shift alphabet greater than 'Z'
+                if(alphabet > 'Z') {
+                    //reshift to starting position
+                    alphabet = (char) (alphabet+'A'-'Z'-1);
+                }
+                ciphertext = ciphertext + alphabet;
+            }
+            else {
+                ciphertext = ciphertext + alphabet;
+            }
+
+        }
+        Files.writeString(output, ciphertext);
+    }
+
+    public static void decodeFile(Path input, Path output, int shift) throws IOException{
+        String ciphertext = Files.readString(input);
+        String decryptMessage = "";
+        for(int i=0; i < ciphertext.length();i++)
+
+        {
+            // Shift one character at a time
+            char alphabet = ciphertext.charAt(i);
+            // if alphabet lies between a and z
+            if(alphabet >= 'a' && alphabet <= 'z')
+            {
+                // shift alphabet
+                alphabet = (char) (alphabet - shift);
+
+                // shift alphabet lesser than 'a'
+                if(alphabet < 'a') {
+                    //reshift to starting position
+                    alphabet = (char) (alphabet-'a'+'z'+1);
+                }
+                decryptMessage = decryptMessage + alphabet;
+            }
+            // if alphabet lies between A and Z
+            else if(alphabet >= 'A' && alphabet <= 'Z')
+            {
+                // shift alphabet
+                alphabet = (char) (alphabet - shift);
+
+                //shift alphabet lesser than 'A'
+                if (alphabet < 'A') {
+                    // reshift to starting position
+                    alphabet = (char) (alphabet-'A'+'Z'+1);
+                }
+                decryptMessage = decryptMessage + alphabet;
+            }
+            else
+            {
+                decryptMessage = decryptMessage + alphabet;
+            }
+        }
+        Files.writeString(output, decryptMessage);
+    }
+}
---
+++ src/com/melnikov/decoder/Runner.java
@@ -0,0 +1,69 @@
+package com.melnikov.decoder;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Do you want to code or decode the message? (1 - code, 2 - decode)");
+
+        if (scanner.nextInt() == 1){
+            System.out.println("Type shift: ");
+            int shift = scanner.nextInt();
+            System.out.println("Type path to text file that you want to code: ");
+            String inputString = scanner.next();
+            String[] inputPathString = inputString.split("/");
+
+            System.out.println("Type path to output file: ");
+            String outputString = scanner.next();
+            String[] outputPathString = outputString.split("/");
+
+            Path input = Path.of(inputPathString[0], getOthers(inputPathString));
+            Path output = Path.of(outputPathString[0], getOthers(outputPathString));
+
+            try {
+                Coder.codeFile(input, output, shift);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        else if (scanner.nextInt() == 2){
+            System.out.println("Type shift: ");
+            int shift = scanner.nextInt();
+            System.out.println("Type path to text file that you want to decode: ");
+            String inputString = scanner.next();
+            String[] inputPathString = inputString.split("/");
+
+            System.out.println("Type path to output file: ");
+            String outputString = scanner.next();
+            String[] outputPathString = outputString.split("/");
+
+            Path input = Path.of(inputPathString[0], getOthers(inputPathString));
+            Path output = Path.of(outputPathString[0], getOthers(outputPathString));
+
+            try {
+                Coder.decodeFile(input, output, shift);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        else {
+            throw new UnsupportedOperationException("You can type only \"1\" or \"2\".");
+        }
+
+
+    }
+
+    private static String[] getOthers(String[] strings){
+        String[] otherInput = new String[strings.length-1];
+        for (int i = 0; i < strings.length-1; i++) {
+            otherInput[i] = strings[i+1];
+        }
+        return otherInput;
+    }
+}
